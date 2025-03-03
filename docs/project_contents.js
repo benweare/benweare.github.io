ae9e37c036088d1751080ee3c9b5e0da8fd9b575
benweare <92488183+benweare@users.noreply.github.com>
2025-03-03 21:25:32 +0000

    updates
    updates to formatting and testing layouts

---
+++ docs/project_contents.js
@@ -0,0 +1,19 @@
+js_badge = "<i class='fab fa-js-square'></i> JavaScript"
+python_badge = "<i class='fab fa-python'></i> Python"
+
+var projects = [
+    {
+        title: "TEM blogs",
+        //badges: [js_badge, python_badge],
+        body: "A gentle introduction to TEM.",
+        link: "https://benweare.github.io/",
+        link_text: "<i class='fa-solid fa-microscope'></i> Read"
+    },
+    {
+        title: "About",
+        //badges: [python_badge, "OpenCV"],
+        body: "About text.",
+        link: "https://benweare.github.io/",
+        link_text: "<i class='fab fa-github'></i> Repo",
+    },
+]
